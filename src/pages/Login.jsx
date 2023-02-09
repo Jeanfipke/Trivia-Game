bcacc398d37ac9b75d508e76d636169b0b37591e
Bruno <brunoalvimduarte@gmail.com>
2023-02-09 17:08:14 -0300

    Req 02 - Creating the play button feature

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Login extends Component {
@@ -12,6 +13,16 @@ class Login extends Component {
     this.setState({
       [name]: value,
     }, this.validation);
+  };
+
+  handlePlayClick = async () => {
+    const apiResult = await fetch('https://opentdb.com/api_token.php?command=request')
+      .then((response) => response.json())
+      .then((data) => data);
+    console.log(apiResult);
+    localStorage.setItem('token', apiResult.token);
+    const { history } = this.props;
+    history.push('/game');
   };
 
   validation = () => {
@@ -44,10 +55,23 @@ class Login extends Component {
           value={ name }
           onChange={ this.handleChange }
         />
-        <button disabled={ isPlayDisabled } data-testid="btn-play">Play</button>
+        <button
+          disabled={ isPlayDisabled }
+          data-testid="btn-play"
+          onClick={ this.handlePlayClick }
+        >
+          Play
+
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
