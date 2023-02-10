0ffd150ea049750e1f8e8fb601efa98026a4bb44
Bruno <brunoalvimduarte@gmail.com>
2023-02-10 00:54:07 -0300

    Req 06 - Creating the game questions

---
+++ src/components/Questions.jsx
@@ -0,0 +1,75 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class Questions extends Component {
+  // componentDidMount() {
+  //   const { questions } = this.props;
+  //   if (questions.length > 0) {
+  //     this.handleSuffleAnswers();
+  //   }
+  // }
+
+  dataTestIdResponsive = (answer) => {
+    const { questions, currQuestion } = this.props;
+    if (answer === questions[currQuestion].correct_answer) {
+      return 'correct-answer';
+    }
+    const findIncorrectAnswerIndex = questions[currQuestion]
+      .incorrect_answers.findIndex((incorrect) => incorrect === answer);
+    console.log(findIncorrectAnswerIndex);
+    return `wrong-answer-${findIncorrectAnswerIndex}`;
+  };
+
+  render() {
+    const { questions, currQuestion, shuffleAnswers } = this.props;
+    if (questions.length === 0 || shuffleAnswers.length === 0) return 'loading...';
+    return (
+      <div data-testid="answer-options">
+        <p data-testid="question-category">{questions[currQuestion].category}</p>
+        <p data-testid="question-text">{questions[currQuestion].question}</p>
+        { questions[currQuestion].type === 'multiple' ? (
+          <div>
+            <button data-testid={ this.dataTestIdResponsive(shuffleAnswers[0]) }>
+              {shuffleAnswers[0]}
+            </button>
+            <button data-testid={ this.dataTestIdResponsive(shuffleAnswers[1]) }>
+              {shuffleAnswers[1]}
+
+            </button>
+            <button data-testid={ this.dataTestIdResponsive(shuffleAnswers[2]) }>
+              {shuffleAnswers[2]}
+
+            </button>
+            <button data-testid={ this.dataTestIdResponsive(shuffleAnswers[3]) }>
+              {shuffleAnswers[3]}
+
+            </button>
+          </div>
+        ) : (
+          <div data-testid="answer-options">
+            <button data-testid={ this.dataTestIdResponsive(shuffleAnswers[0]) }>
+              {shuffleAnswers[0]}
+            </button>
+            <button data-testid={ this.dataTestIdResponsive(shuffleAnswers[1]) }>
+              {shuffleAnswers[1]}
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Questions.propTypes = {
+  currQuestion: PropTypes.number.isRequired,
+  questions: PropTypes.arrayOf(PropTypes.shape({
+    incorrect_answers: PropTypes.arrayOf(PropTypes.string),
+    correct_answer: PropTypes.string,
+    category: PropTypes.string,
+    question: PropTypes.string,
+    type: PropTypes.string,
+  })).isRequired,
+  shuffleAnswers: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Questions;
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,14 +1,59 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Questions from '../components/Questions';
 
 class Game extends Component {
+  state = {
+    questions: [],
+    currQuestion: 0,
+    shuffleAnswers: [],
+  };
+
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    const { history } = this.props;
+    fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.response_code === 0) {
+          this.setState({ questions: data.results }, this.handleSuffleAnswers);
+          return;
+        }
+        localStorage.removeItem('token');
+        history.push('/');
+      });
+  }
+
+  handleSuffleAnswers = () => {
+    const { questions, currQuestion } = this.state;
+    const answers = [...questions[currQuestion].incorrect_answers,
+      questions[currQuestion].correct_answer];
+    const bla = 0.5;
+    const shuffleAnswers = answers.sort(() => Math.random() - bla);
+    this.setState({ shuffleAnswers });
+  };
+
   render() {
+    const { questions, currQuestion, shuffleAnswers } = this.state;
     return (
       <div>
         <Header />
+        <Questions
+          questions={ questions }
+          currQuestion={ currQuestion }
+          shuffleAnswers={ shuffleAnswers }
+        />
       </div>
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Game;
